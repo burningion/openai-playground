43b1e38df26baf1779b1904e3947772ccacc054a
KP Kaiser <kirk@zothcorp.com>
2023-04-04 16:01:19 +0000

    add set prompt context and initial prompt

--- playground.py
+++ playground.py
@@ -6,15 +6,21 @@ from rich import print
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 messagesList = []
-messagesList.append({"role": "system", "content": "you are an experienced python backend developer who cares deeply about security"})
-messagesList.append({"role": "user", "content": "write me a fastapi python program to accept file uploads and store them on amazon s3"})
+print("Enter your system prompt context below. As an example it should be something like: \n'you are an experienced frontend developer who cares about readability'")
+system_prompt = input("Leave blank for default: ")
+if system_prompt == "":
+    system_prompt = "you are an experienced frontend developer who cares deeply about code readability"
+messagesList.append({"role": "system", "content": system_prompt})
+first_prompt = input("Enter your prompt: ")
+messagesList.append({"role": "user", "content": first_prompt})
+
 response = openai.ChatCompletion.create(model="gpt-4", messages=messagesList)
 
 resContent = response["choices"][0]["message"]["content"]
 print(resContent)
 messagesList.append({"role": "assistant", "content": resContent})
 while True:
-    next_prompt = input("enter next prompt (q to quit): ")
+    next_prompt = input("Enter next prompt (q to quit): ")
     if next_prompt == "q":
         import csv
         with open('chatlog.csv', 'w') as csvfile:
@@ -22,6 +28,7 @@ while True:
             writer.writeheader()
             for message in messagesList:
                 writer.writerow(message)
+        print("Wrote log of chat to `chatlog.csv`")
         exit()
     messagesList.append({"role": "user", "content": str(next_prompt)})
     response = openai.ChatCompletion.create(model="gpt-4", messages=messagesList)
